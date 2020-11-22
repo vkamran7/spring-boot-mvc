f6a730b91d50e0535af973adcc7303f044654320
Kamran <vkamran7@gmail.com>
2020-11-22 21:36:29 +0400

    Implemented Delete for Countries

--- src/main/java/az/admin/fleetap/controller/CountryController.java
+++ src/main/java/az/admin/fleetap/controller/CountryController.java
@@ -1,13 +1,52 @@
 package az.admin.fleetap.controller;
 
+import az.admin.fleetap.model.Country;
+import az.admin.fleetap.service.CountryService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class CountryController {
 
+    private CountryService countryService;
+
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
     @GetMapping("/countries")
-    public String getCountries() {
+    public String getCountries(Model model) {
+        List<Country> countries = countryService.getCountries();
+        model.addAttribute("countries", countries);
         return "country";
+    }
+
+    @PostMapping("/countries/addNew")
+    public String addNew(Country country) {
+        countryService.save(country);
+        return "redirect:/countries";
+    }
+
+    @GetMapping("/countries/findById")
+    @ResponseBody
+    public Optional<Country> findById(int id) {
+        return countryService.findById(id);
+    }
+
+
+    @RequestMapping(value = "/countries/update", method = {RequestMethod.GET, RequestMethod.PUT})
+    public String update(Country country) {
+        countryService.save(country);
+        return "redirect:/countries";
+    }
+
+    @RequestMapping(value = "/countries/delete", method = {RequestMethod.GET, RequestMethod.DELETE})
+    public String delete(int id) {
+        countryService.delete(id);
+        return "redirect:/countries";
     }
 }
--- src/main/java/az/admin/fleetap/service/CountryService.java
+++ src/main/java/az/admin/fleetap/service/CountryService.java
@@ -1,7 +1,34 @@
 package az.admin.fleetap.service;
 
+import az.admin.fleetap.model.Country;
+import az.admin.fleetap.repository.CountryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CountryService {
+
+    private final CountryRepository countryRepository;
+
+    public CountryService(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    public List<Country> getCountries() {
+        return countryRepository.findAll();
+    }
+
+    public void save(Country country) {
+        countryRepository.save(country);
+    }
+
+    public Optional<Country> findById(Integer id) {
+        return countryRepository.findById(id);
+    }
+
+    public void delete(Integer id) {
+        countryRepository.deleteById(id);
+    }
 }
