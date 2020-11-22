c94542eadd76cfbefc078edc6f88d645830fe826
Kamran <vkamran7@gmail.com>
2020-11-22 22:10:14 +0400

    Implemented StateController and StateService

--- src/main/java/az/admin/fleetap/controller/StateController.java
+++ src/main/java/az/admin/fleetap/controller/StateController.java
@@ -1,12 +1,53 @@
 package az.admin.fleetap.controller;
 
+import az.admin.fleetap.model.Country;
+import az.admin.fleetap.model.State;
+import az.admin.fleetap.service.CountryService;
+import az.admin.fleetap.service.StateService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class StateController {
-    @GetMapping("/state")
-    public String getCountries() {
+    private StateService stateService;
+
+    public StateController(StateService stateService) {
+        this.stateService = stateService;
+    }
+
+    @GetMapping("/states")
+    public String getStates(Model model) {
+        List<State> states = stateService.getStates();
+        model.addAttribute("states", states);
         return "state";
+    }
+
+    @PostMapping("/states/addNew")
+    public String addNew(State state) {
+        stateService.save(state);
+        return "redirect:/states";
+    }
+
+    @GetMapping("/states/findById")
+    @ResponseBody
+    public Optional<State> findById(int id) {
+        return stateService.findById(id);
+    }
+
+
+    @RequestMapping(value = "/states/update", method = {RequestMethod.GET, RequestMethod.PUT})
+    public String update(State state) {
+        stateService.save(state);
+        return "redirect:/states";
+    }
+
+    @RequestMapping(value = "/states/delete", method = {RequestMethod.GET, RequestMethod.DELETE})
+    public String delete(int id) {
+        stateService.delete(id);
+        return "redirect:/states";
     }
 }
--- src/main/java/az/admin/fleetap/service/StateService.java
+++ src/main/java/az/admin/fleetap/service/StateService.java
@@ -1,7 +1,34 @@
 package az.admin.fleetap.service;
 
+import az.admin.fleetap.model.State;
+import az.admin.fleetap.repository.StateRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StateService {
+
+    private final StateRepository stateRepository;
+
+    public StateService(StateRepository stateRepository) {
+        this.stateRepository = stateRepository;
+    }
+
+    public List<State> getStates() {
+        return stateRepository.findAll();
+    }
+
+    public void save(State state) {
+        stateRepository.save(state);
+    }
+
+    public Optional<State> findById(int id) {
+        return stateRepository.findById(id);
+    }
+
+    public void delete(int id) {
+        stateRepository.deleteById(id);
+    }
 }
